3f3736ce584cea9155c9fa8df4c1a029d3da2a74
Khoa <dangkhoa123bn@gmail.com>
2022-03-05 12:41:15 +0700

    first commit

---
+++ ui/BaseService/BusinessLogic.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BaseService
+{
+    public static class BusinessLogic
+    {
+        public static string ProcessingContent(this string contents)
+        {
+            if ((contents.Contains("Nhập dấu")
+                || contents.Contains("nhập dấu"))
+                && !contents.Contains("Nhập dấu.")
+                && !contents.Contains("nhập dấu."))
+            {
+                contents = contents.Replace("Nhập dấu chấm phẩy.", "; ");
+                contents = contents.Replace(" nhập dấu chấm phẩy.", "; ");
+                contents = contents.Replace("Nhập dấu chấm phẩy ", "; ");
+                contents = contents.Replace(" nhập dấu chấm phẩy ", "; ");
+
+                //dấu chấm hỏi
+                contents = contents.Replace("Nhập dấu chấm hỏi.", "? ");
+                contents = contents.Replace(" nhập dấu chấm hỏi.", "? ");
+                contents = contents.Replace("Nhập dấu chấm hỏi ", "? ");
+                contents = contents.Replace(" nhập dấu chấm hỏi ", "? ");
+
+                //dấu chấm than
+                contents = contents.Replace("Nhập dấu chấm than.", "! ");
+                contents = contents.Replace(" nhập dấu chấm than.", "! ");
+                contents = contents.Replace("Nhập dấu chấm than ", "! ");
+                contents = contents.Replace(" nhập dấu chấm than ", "! ");
+
+                //dấu chấm
+                contents = contents.Replace("Nhập dấu chấm.", ". ");
+                contents = contents.Replace(" nhập dấu chấm.", ". ");
+                contents = contents.Replace("Nhập dấu chấm ", ". ");
+                contents = contents.Replace(" nhập dấu chấm ", ". ");
+
+                //dấu phẩy
+                contents = contents.Replace("Nhập dấu phẩy.", ", ");
+                contents = contents.Replace(" nhập dấu phẩy.", ", ");
+                contents = contents.Replace("Nhập dấu phẩy", ", ");
+                contents = contents.Replace(" nhập dấu phẩy ", ", ");
+
+                //dấu xuống dòng
+                contents = contents.Replace("Nhập dấu xuống dòng.", "\n");
+                contents = contents.Replace(" nhập dấu xuống dòng.", "\n");
+                contents = contents.Replace("Nhập dấu xuống dòng ", "\n");
+                contents = contents.Replace(" nhập dấu xuống dòng ", "\n");
+
+                //dấu hai chấm
+                contents = contents.Replace("Nhập dấu hai chấm.", ": ");
+                contents = contents.Replace(" nhập dấu hai chấm.", ": ");
+                contents = contents.Replace("Nhập dấu hai chấm", ": ");
+                contents = contents.Replace(" nhập dấu hai chấm ", ": ");
+
+                //dấu gạch ngang 
+                contents = contents.Replace("Nhập dấu gạch ngang.", "- ");
+                contents = contents.Replace(" nhập dấu gạch ngang.", "- ");
+                contents = contents.Replace("Nhập dấu gạch ngang", "- ");
+                contents = contents.Replace(" nhập dấu gạch ngang ", "- ");
+            }
+
+         
+            // viết hoa chữ cái đầu tiên
+            contents = char.ToUpper(contents[0]) + contents.Substring(1);
+
+
+            // viết hoa sau dấu câu 
+            string[] punctuators = { "?", "!", ",", ":", ";", "." };
+            for (int i = 0; i < 6; i++)
+            {
+                int pos = contents.IndexOf(punctuators[i]);
+                while (pos != -1 && pos < contents.Length - 2)
+                {
+                    contents = contents.Insert(pos + 2, char.ToUpper(contents[pos + 2]).ToString());
+                    contents = contents.Remove(pos + 3, 1);
+                    pos = contents.IndexOf(punctuators[i], pos + 1);
+                }
+            }
+            return contents;
+        }
+
+    }
+}
---
+++ ui/ui/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BaseService;
+
+namespace ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SpeechService _speechService;
+        private TaskCompletionSource<int> _stopTaskCompletionSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TestProcessingContent();
+            //_speechService = new SpeechService();
+            //_speechService.SetConfig("a11e89949b6543bb83fe92e30c04b67b", "eastus", "vi-VN");
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stopTaskCompletionSource = new TaskCompletionSource<int>();
+            Task.Run(async () => { await _speechService.Start(_stopTaskCompletionSource, RecognitionCallback).ConfigureAwait(false); });
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            ClearButton.IsEnabled = false;
+        }
+
+        private void StopButton_OnClickopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _stopTaskCompletionSource.TrySetResult(0);
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+            ClearButton.IsEnabled=true;
+        }
+
+        private void RecognitionCallback(string result)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                DisplayText.Text += result;
+            });
+        }
+
+        private void FromMic_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FromFile_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayText.Text = "";
+
+        }
+
+        private void TestProcessingContent()
+        {
+            DisplayText.Text = "ádkajshdajk nhập dấu.".ProcessingContent();
+        }
+    }
+}
